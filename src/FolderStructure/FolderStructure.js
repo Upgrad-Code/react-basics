3f88a26de35f7777edfad509e52118aa95a516cf
Upgrad-Code <112937791+Upgrad-Code@users.noreply.github.com>
2022-09-16 18:24:46 +0530

    1 file modified

--- src/FolderStructure/FolderStructure.js
+++ src/FolderStructure/FolderStructure.js
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 
 const FolderStructure = ({ explorer }) => {
-  return <span>{explorer.name}</span>;
+  const [toggle, setToggle] = useState(false);
+  const hasChildren = explorer.items && explorer.items.length;
+  return (
+    <>
+      <span className="px-2" onClick={() => setToggle(!toggle)}>
+        {explorer.name}
+      </span>
+      <div className="px-4" style={{ display: toggle ? 'block' : 'none' }}>
+        {hasChildren &&
+          explorer.items.map((item) => (
+            <FolderStructure key={item.name} explorer={item} />
+          ))}
+      </div>
+    </>
+  );
 };
 
 export default FolderStructure;
